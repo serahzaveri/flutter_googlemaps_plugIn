2cddab07e8875cc2d76fc93339ddf29aba3ead01
serahios <serah.zaveri@gmail.com>
2020-08-05 01:26:27 -0400

    hide key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyC_zl_ZDYuCLTb20duz8T9VaochcXg3CSM")
+    GMSServices.provideAPIKey("INSERT API KEY HERE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
